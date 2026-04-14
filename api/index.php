e0205c84a4a4f1bacc6ef2c8a478767270b38b77
ekyaaa <ekyamuhammad@gmail.com>
2026-04-14 21:13:20 +0700

    asdasd

--- api/index.php
+++ api/index.php
@@ -1,23 +1,22 @@
 <?php
 
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
-require __DIR__ . '/../vendor/autoload.php';
+define('LARAVEL_START', microtime(true));
 
+// 1. Load Autoload & App
+require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Paksa Laravel mendaftarkan ViewServiceProvider jika belum
-if (!$app->bound('view')) {
-    $app->register(Illuminate\View\ViewServiceProvider::class);
-    $app->register(Illuminate\Events\EventServiceProvider::class);
-}
-
+// 2. Paksa Storage Path ke /tmp
 $app->useStoragePath('/tmp');
 
-try {
-    $app->handleRequest(Request::capture());
-} catch (\Exception $e) {
-    echo "<h1>Fatal Error Detected:</h1>";
-    echo "<pre>" . $e->getMessage() . "</pre>";
-    echo "<p>File: " . $e->getFile() . "</p>";
-}
+// 3. Jalankan Kernel secara Manual
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
