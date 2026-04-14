66b965d5257ae942cdeae2a8e2b123ab9f3964e9
ekyaaa <ekyamuhammad@gmail.com>
2026-04-14 21:22:56 +0700

    asd

--- api/index.php
+++ api/index.php
@@ -1,15 +1,20 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// Bootstrapping aplikasi
+// Coba memuat aplikasi
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Paksa folder storage ke /tmp
+// JIka $app gagal dimuat, kita akan tahu di sini
+if (!$app) {
+    die("Gagal memuat bootstrap/app.php");
+}
+
 $app->useStoragePath('/tmp');
 
-// Jalankan aplikasi
+// Tambahkan echo untuk test
+// echo "Laravel Booted!"; 
+
 $app->handleRequest(Request::capture());
