33f4cbadf93d8feabd0781ae8d3ff22ad8180aba
ekyaaa <ekyamuhammad@gmail.com>
2026-04-14 21:00:29 +0700

    update vercel

--- api/index.php
+++ api/index.php
@@ -1,20 +1,12 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
-define('LARAVEL_START', microtime(true));
+require __DIR__ . '/../vendor/autoload.php';
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+// Memaksa Laravel menggunakan folder /tmp untuk cache/views karena Vercel Read-Only
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app->useStoragePath('/tmp');
 
 $app->handleRequest(Request::capture());
