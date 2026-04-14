10994092113134d117fc974aed6eb09f086c7b71
ekyaaa <ekyamuhammad@gmail.com>
2026-04-14 21:19:52 +0700

    asd

--- api/index.php
+++ api/index.php
@@ -1,22 +1,15 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
-define('LARAVEL_START', microtime(true));
-
-// 1. Load Autoload & App
 require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrapping aplikasi
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// 2. Paksa Storage Path ke /tmp
+// Paksa folder storage ke /tmp
 $app->useStoragePath('/tmp');
 
-// 3. Jalankan Kernel secara Manual
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+// Jalankan aplikasi
+$app->handleRequest(Request::capture());
